a054b990d2f3cbfc10e9e4b5c9377717c4379437
convict-git <official.mr.convict@gmail.com>
2021-08-06 18:12:37 +0530

    Changed the time for setTimeOut

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -4,7 +4,9 @@ export const useDebounce = (input: string) => {
   const [output, setOutput] = React.useState<string>('');
   React.useEffect(() => {
     const timeoutId: ReturnType<typeof setTimeout> = setTimeout(() => {
-      setOutput(input);
+      if (input) {
+        setOutput(input);
+      }
     }, 500);
     return () => clearTimeout(timeoutId);
   });
