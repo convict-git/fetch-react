74a5e69fe474dca2758212b406871f7d80162165
convict-git <official.mr.convict@gmail.com>
2021-08-06 13:52:40 +0530

    Demo OutputBar to check debounce

---
+++ src/components/OutputBar/OutputBar.tsx
@@ -0,0 +1,10 @@
+// import {
+//   ContestantCard,
+//   ContestantCardWait,
+// } from './ContestantCard/ContestantCard';
+
+const OutputBar = ({ contestId }: { contestId: string }) => {
+  return <>{contestId}</>;
+};
+
+export default OutputBar;
