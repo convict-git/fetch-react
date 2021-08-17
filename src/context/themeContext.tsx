5ba1ad268821986e2f095302a161881a869b54b1
convict-git <official.mr.convict@gmail.com>
2021-08-17 23:07:11 +0530

    themeContext

---
+++ src/context/themeContext.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const colors = {
+  dark: {
+    'bg-color': 'rgb(32, 4, 68)',
+    'fg-color': 'whitesmoke',
+  },
+  light: {
+    'bg-color': 'whitesmoke',
+    'fg-color': 'rgb(32, 4, 68)',
+  },
+};
+
+interface ThemeType {
+  mode: 'dark' | 'light';
+  'bg-color': string;
+  'fg-color': string;
+}
+
+const ThemeContext = React.createContext(null);
+
+const themeReducer = (state: ThemeType, action: string): ThemeType => {
+  switch (action) {
+    case 'TOGGLE': {
+      return state.mode === 'dark'
+        ? { mode: 'light', ...colors['light'] }
+        : { mode: 'dark', ...colors['dark'] };
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action}`);
+    }
+  }
+};
+
+const ThemeProvider = (props: any) => {
+  const [themeData, dispachThemeData] = React.useReducer(themeReducer, {
+    mode: 'light',
+    ...colors['light'],
+  });
+
+  return (
+    <ThemeContext.Provider value={[themeData, dispachThemeData]} {...props} />
+  );
+};
+
+const useTheme = () => {
+  const context = React.useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be used within ThemeProvider');
+  }
+  return context;
+};
+
+export { ThemeProvider, useTheme };
+export type { ThemeType };
--- src/shared/constansts.ts
+++ src/shared/constansts.ts
@@ -2,7 +2,7 @@ export const CONST = {
   contestId: 1548,
   from: 1,
   count: 9,
-  retryCount: 1,
+  retryCount: 5,
   retryTime: 2000,
   viewCards: false,
 };
